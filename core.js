365c0f5ab5c302680a0747fb9caf9b895d28998a
Oignons <atome.corporation@gmail.com>
2013-05-22 16:30:22 +0200

    First commit.

---
+++ core.js
@@ -0,0 +1,30 @@
+// Print scroll
+$(function(){
+
+	// Display the list of servers :
+	$.getJSON('https://api.guildwars2.com/v1/world_names.json', function(data) {
+			var i = 0;
+
+			// Clear the select
+			$('#servers_list').find('option').remove('');
+
+			$(data).each(function(){
+				$('#servers_list').append('<option value='+data[i]['id']+'>'+data[i]['name']+'</option>')
+				i++;
+			});
+	});
+});
+
+function load_events() {
+	// Display events 
+	$('#results').html('Loading events ...');
+
+	get_gw2_events($('#servers_list').find(':selected').val(), function(events_list) {
+		var results_html = "";
+		console.log(events_list);
+		for(var i=0; i<events_list.length; i++) {
+			results_html += '<br/>'+events_list[i][1]+' : On '+events_list[i][2]+' : '+events_list[i][0];
+		}
+		$('#results').html(results_html);
+	});
+}
